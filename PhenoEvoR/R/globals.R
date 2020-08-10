7f2292fb9b7fd92586668d35a54a12ba0e7ed6c0
Jessica Audrey Lee <jessica.lee@globalviral.org>
2020-08-10 10:55:04 -0700

    new R package and sample data!

---
+++ PhenoEvoR/R/globals.R
@@ -0,0 +1,5 @@
+globalVariables(c('run.number','timepoint','abundance','barcode',
+                  'step','mean.toxin','count.turtles','mean.degrade.rate',
+                  'degrade.rate','mean.generation','mean.response.error',
+                  'response.error','generation','switch.rate','mean.switch.rate',
+                  'hist'))
