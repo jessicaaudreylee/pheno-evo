1ffad34c9af981501b7fae9ceb37b19c8890af1d
Jessica Audrey Lee <jessica.lee@globalviral.org>
2020-08-10 11:28:37 -0700

    silly change - moving R package

--- PhenoEvoR/R/globals.R
+++
@@ -1,5 +0,0 @@
-globalVariables(c('run.number','timepoint','abundance','barcode',
-                  'step','mean.toxin','count.turtles','mean.degrade.rate',
-                  'degrade.rate','mean.generation','mean.response.error',
-                  'response.error','generation','switch.rate','mean.switch.rate',
-                  'hist'))
